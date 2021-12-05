579f39f7c10644c2f4dae329edbaa3b40e816d20
Nick Land <land.d.nicholas@gmail.com>
2021-12-05 18:36:04 -0500

    ✨ Add changes to header

---
+++ components/Header/Header.test.tsx
@@ -0,0 +1,36 @@
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import { Header } from "./Header";
+
+const useRouter = jest.spyOn(require("next/router"), "useRouter");
+
+let router;
+
+describe("Header", () => {
+  beforeEach(() => {
+    router = { push: jest.fn(), pathname: "/about" };
+    useRouter.mockReturnValue(router);
+  });
+
+  it("renders 'Nick Land' in the header", () => {
+    render(<Header />);
+
+    expect(screen.getByText(/nick land/i)).toBeInTheDocument();
+  });
+
+  it("sets 'text-green-300' class on the active page", () => {
+    render(<Header />);
+
+    const aboutButton = screen.getByRole("button", { name: /about/i });
+
+    expect(aboutButton).toHaveClass("text-green-300");
+  });
+
+  it("does not set 'text-green-300' on the page that is not active", () => {
+    render(<Header />);
+
+    const contactButton = screen.getByRole("button", { name: /contact/i });
+
+    expect(contactButton).not.toHaveClass("text-green-300");
+  });
+});
